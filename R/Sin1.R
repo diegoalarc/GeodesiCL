5d7e817d0d8ad14b1a7f9118c2e301e0a9161f5f
diegoalarc <diego.alarcondiaz@gmail.com>
2020-12-08 18:59:44 +0100

    Fix description

--- R/Sin1.R
+++ R/Sin1.R
@@ -2,7 +2,6 @@
 #'
 #' Contains the values of sin 1".
 #' @format A \code{numeric} with the value of sin 1"., which is:
-#' \describe{
-#' \item{Sin_1}{Value of sin 1".}
+#' @param Sin_1 Value of sin 1".
 #' }
 "Sin_1"
