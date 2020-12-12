3fb19999e8a297daf337100417fdc337641e2086
diegoalarc <diego.alarcondiaz@gmail.com>
2020-12-12 14:52:41 +0100

    Update dependency packages

--- R/globals.R
+++ R/globals.R
@@ -4,3 +4,27 @@ utils::globalVariables(c("Ellipsoids", "Sin_1"))
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+#' @importFrom sp SpatialPoints
+#' @export
+sp::SpatialPoints
+
+#' @importFrom sp spTransform
+#' @export
+sp::spTransform
+
+#' @importFrom sp proj4string
+#' @export
+sp::proj4string
+
+#' @importFrom sp coordinates
+#' @export
+sp::coordinates
+
+#' @importFrom sp CRS
+#' @export
+sp::CRS
+
+#' @importFrom dplyr mutate
+#' @export
+dplyr::mutate
